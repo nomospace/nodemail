5b9a4ed450e4ba1a0b5932202f34b14c7901abe5
nomospace <jinlu_hz@163.com>
2012-09-04 23:26:41 +0800

    remove msg-raw temp files

--- controllers/sign.js
+++ controllers/sign.js
@@ -3,59 +3,58 @@ var sanitize = require('validator').sanitize;
 var crypto = require('crypto');
 
 exports.showLogin = function(req, res) {
-	// req.session._loginReferer = req.headers.referer;
-	res.render('sign/signin.html');
+  // req.session._loginReferer = req.headers.referer;
+  res.render('sign/signin.html');
 }
 
 exports.login = function(req, res, next) {
-	var name = sanitize(req.body.name).trim().toLowerCase();
-	var pass = sanitize(req.body.pass).trim();
+  var name = sanitize(req.body.name).trim().toLowerCase();
+  var pass = sanitize(req.body.pass).trim();
 
-	var user = {
-		name: name,
-		pass: pass
-	};
+  var user = {
+    name: name,
+    pass: pass
+  };
 
-	// store session cookie
-	genSession(user, res);
+  // store session cookie
+  genSession(user, res);
 
-	req.session.user = user;
-	res.local('currentUser', req.session.user);
+  req.session.user = user;
+  res.local('currentUser', req.session.user);
 
-	res.redirect('/mail');
+  res.redirect('/mail');
 }
 
 exports.signout = function(req, res, next) {
-	req.session.destroy();
-	res.clearCookie(config.authCookieName, {
-		path: '/'
-	});
-	res.redirect(req.headers.referer || 'home');
+  req.session.destroy();
+  res.clearCookie(config.authCookieName, {
+    path: '/'
+  });
+  res.redirect(req.headers.referer || 'home');
 }
 
 exports.authUser = function(req, res, next) {
-	if (req.session.user) {
-		res.local('currentUser', req.session.user);
-	}
-	return next();
+  if (req.session.user) {
+    res.local('currentUser', req.session.user);
+  }
+  return next();
 }
 
 
 // private
 
-
 function genSession(user, res) {
-	var authToken = encrypt(user.name + '\t' + user.pass, config.sessionSecret);
-	// cookie 有效期30天   
-	res.cookie(config.authCookieName, authToken, {
-		path: '/',
-		maxAge: 1000 * 60 * 60 * 24 * 30
-	});
+  var authToken = encrypt(user.name + '\t' + user.pass, config.sessionSecret);
+  // cookie 有效期30天   
+  res.cookie(config.authCookieName, authToken, {
+    path: '/',
+    maxAge: 1000 * 60 * 60 * 24 * 30
+  });
 }
 
 function encrypt(str, secret) {
-	var cipher = crypto.createCipher('aes192', secret);
-	var enc = cipher.update(str, 'utf8', 'hex');
-	enc += cipher.final('hex');
-	return enc;
+  var cipher = crypto.createCipher('aes192', secret);
+  var enc = cipher.update(str, 'utf8', 'hex');
+  enc += cipher.final('hex');
+  return enc;
 }
