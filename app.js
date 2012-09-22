42cb26a32346e515f80db27ba8a5ac0d9de159b0
nomospace <jinlu_hz@163.com>
2012-09-23 01:28:45 +0800

    update package.json

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var path = require('path');
 var express = require('express');
 var config = require('./config').config;
 var routes = require('./routes');
-var markdown = require('markdown-js');
+//var markdown = require('markdown-js');
 var partials = require('express-partials');
+var ejs = require('ejs');
 
 var app = express();
 
@@ -15,20 +16,20 @@ app.configure(function() {
   app.set('views engine', 'html');
   //  启用 View 缓存（在开发阶段被关闭）
   app.set('view cache', false);
-  app.engine('html', require('ejs').renderFile);
-  app.engine('md', function(path, options, fn) {
-    fs.readFile(path, 'utf8', function(err, str) {
-      if (err) return fn(err);
-      // str = markdown.parse(str).toString();
-      var html = markdown.makeHtml(str);
-      return function(locals) {
-        return html.replace(/\{([^}]+)\}/g, function(_, name) {
-          return locals[name];
-        });
-      };
-      // fn(null, str);
-    });
-  });
+  app.engine('html', ejs.renderFile);
+//  app.engine('md', function(path, options, fn) {
+//    fs.readFile(path, 'utf8', function(err, str) {
+//      if (err) return fn(err);
+//      // str = markdown.parse(str).toString();
+//      var html = markdown.makeHtml(str);
+//      return function(locals) {
+//        return html.replace(/\{([^}]+)\}/g, function(_, name) {
+//          return locals[name];
+//        });
+//      };
+//      // fn(null, str);
+//    });
+//  });
   // app.engine('.md', {
   //   compile: function(str, options) {
   //     var html = markdown.makeHtml(str);
@@ -49,7 +50,6 @@ app.configure(function() {
   app.use(require('./controllers/sign').authUser);
   // load the express-partials middleware
   app.use(partials());
-
 });
 
 // app.error(function(err, req, res) {
@@ -75,12 +75,12 @@ app.listen(config.port);
 // 设置静态资源根目录
 var staticDir = path.join(__dirname, 'public');
 // app.configure('development', function() {
-  app.use(express.static(staticDir));
-  // 在 HTML 页面中显示程序传递和抛出的异常
-  app.use(express.errorHandler({
-    dumpExceptions: true,
-    showStack: true
-  }));
+app.use(express.static(staticDir));
+// 在 HTML 页面中显示程序传递和抛出的异常
+app.use(express.errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
 // });
 
 console.log(config.host + ':' + config.port);
